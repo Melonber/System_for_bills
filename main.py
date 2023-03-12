1bbbfdb21a9e06536e8d50b11d8b35fb528b010b
Беркман Айдын <68331365+Melonber@users.noreply.github.com>
2023-03-12 16:33:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,19 @@ def get_conn():
 @bot.message_handler(commands=['add'])
 def add_email(message):
     if message.from_user.username in users:
-        try:
-            # Получение почты и пароля от пользователя
-            email = message.text.split()[1]
-            password = message.text.split()[2]
-            # Создание соединения и курсора
-            conn = get_conn()
-            cursor = conn.cursor()
-            # Добавление в базу данных
-            cursor.execute("INSERT INTO emails (email, password) VALUES (?, ?)", (email, password))
-            conn.commit()
-            # Закрытие соединения
-            cursor.close()
-            conn.close()
-            bot.send_message(message.chat.id, f"Email {email} с паролем {password} добавлен в базу данных")
-        except:
-            bot.send_message(message.chat.id, "Неправильный формат ввода")
-
+        # Получение почты и пароля от пользователя
+        email = message.text.split()[1]
+        password = message.text.split()[2]
+        # Создание соединения и курсора
+        conn = get_conn()
+        cursor = conn.cursor()
+        # Добавление в базу данных
+        cursor.execute("INSERT INTO emails (email, password) VALUES (?, ?)", (email, password))
+        conn.commit()
+        # Закрытие соединения
+        cursor.close()
+        conn.close()
+        bot.send_message(message.chat.id, f"Email {email} с паролем {password} добавлен в базу данных")
     else:
         bot.send_message(message.chat.id, '❌ Доступ запрещен ! ❌')
 
@@ -95,28 +91,29 @@ def list_emails(message):
 @bot.message_handler(commands=['use'])
 def get_password(message):
     if message.from_user.username in users:
-        try:
-            # Получение почты, для которой нужно получить пароль
-            email = message.text.split()[1]
-            # Создание соединения и курсора
-            conn = get_conn()
-            cursor = conn.cursor()
-            # Получение пароля из базы данных
-            cursor.execute("SELECT password FROM emails WHERE email=?", (email,))
-            result = cursor.fetchone()
-            # Закрытие соединения
-            cursor.close()
-            conn.close()
-            if result:
-                password = result[0]
-                bot.send_message(message.chat.id, f"Пароль для почты {email}: {password}")
-            else:
-                bot.send_message(message.chat.id, f"Почта {email} не найдена в базе данных. Ипользуй команду /add")
-        except:
-            bot.send_message(message.chat.id, f"Неизвестная ошибка")
-
+        # Получение почты, для которой нужно получить пароль
+        email = message.text.split()[0]
+        # Создание соединения и курсора
+        conn = get_conn()
+        cursor = conn.cursor()
+        # Получение пароля из базы данных
+        cursor.execute("SELECT password FROM emails WHERE email=?", (email,))
+        result = cursor.fetchone()
+        # Закрытие соединения
+        cursor.close()
+        conn.close()
+        if result:
+            password = result[0]
+            bot.send_message(message.chat.id, f"Почта успешно заменена на {email}")
+            global mail_log
+            mail_log = email
+            global pass_email
+            pass_email = password
+        else:
+            bot.send_message(message.chat.id, f"Почта {email} не найдена в базе данных. Можешь добавить её тут: @mail_base_umoney_bot")
     else:
         bot.send_message(message.chat.id, '❌ Доступ запрещен !❌ ')
+
 
 
 # --------------------------------------------------------------------
@@ -168,59 +165,62 @@ def func(message):
     elif (message.text == "🏁"):
         if message.from_user.username in users:
             flag = True
+            try:
+                bot.send_message(message.chat.id, 'Запускаю шарманку !')
+                while flag:
+                        time.sleep(1)
+                        mail = connect_imap()
+                        mail.select('inbox')
+                        status, messages = mail.search(None, 'UNSEEN')
+                        message_ids = messages[0].split()
 
-            bot.send_message(message.chat.id, 'Запускаю шарманку !')
-            while flag:
-                    time.sleep(1)
-                    mail = connect_imap()
-                    mail.select('inbox')
-                    status, messages = mail.search(None, 'UNSEEN')
-                    message_ids = messages[0].split()
+                        body = ''
 
-                    body = ''
+                        for id in message_ids:
+                            status, msg_data = mail.fetch(id, '(RFC822)')
+                            for response_part in msg_data:
+                                if isinstance(response_part, tuple):
+                                    msg = email.message_from_bytes(response_part[1])
+                                    if msg.is_multipart():
+                                        for payload in msg.get_payload():
+                                            if payload.get_content_type() == 'text/plain':
+                                                body = payload.get_payload(decode=True).decode('utf-8')
+                                    else:
+                                        body = msg.get_payload(decode=True).decode('utf-8')
 
-                    for id in message_ids:
-                        status, msg_data = mail.fetch(id, '(RFC822)')
-                        for response_part in msg_data:
-                            if isinstance(response_part, tuple):
-                                msg = email.message_from_bytes(response_part[1])
-                                if msg.is_multipart():
-                                    for payload in msg.get_payload():
-                                        if payload.get_content_type() == 'text/plain':
-                                            body = payload.get_payload(decode=True).decode('utf-8')
-                                else:
-                                    body = msg.get_payload(decode=True).decode('utf-8')
+                        if 'В кошелёк пришли деньги' in body:
+                            start_index = body.find('В кошелёк пришли деньги')
+                            end_index = body.find('Все детали — в истории событий')
 
-                    if 'В кошелёк пришли деньги' in body:
-                        start_index = body.find('В кошелёк пришли деньги')
-                        end_index = body.find('Все детали — в истории событий')
+                            if start_index >= 0 and end_index >= 0:
+                                result = body[start_index:end_index]
+                                result = result.replace('В кошелёк пришли деньги','')
+                                result = result.replace('Способ пополнения Сбербанк, пополнение','')
+                                bot.send_message(message.chat.id, text=result)
 
-                        if start_index >= 0 and end_index >= 0:
-                            result = body[start_index:end_index]
-                            result = result.replace('В кошелёк пришли деньги','')
-                            result = result.replace('Способ пополнения Сбербанк, пополнение','')
-                            bot.send_message(message.chat.id, text=result)
+                                came = re.findall(r"(?<=Пришло\s).*(?=\s₽)", result)
+                                came = came[0]
+                                came = came.replace(' ', '')
+                                global come_amount
+                                come_amount += int(came)
 
-                            came = re.findall(r"(?<=Пришло\s).*(?=\s₽)", result)
-                            came = came[0]
-                            came = came.replace(' ', '')
-                            global come_amount
-                            come_amount += int(came)
+                                bot.send_message(message.chat.id, f"Сумма заявки:  {come_amount}/{vadim_sum}")
 
-                            bot.send_message(message.chat.id, f"Сумма заявки:  {come_amount}/{vadim_sum}")
+                                if come_amount >= vadim_sum:
+                                    bot.send_message(message.chat.id, '🤑 Финиш🏁')
+                                    come_amount = 0
+                                    break
 
-                            if come_amount >= vadim_sum:
-                                bot.send_message(message.chat.id, '🤑 Финиш🏁')
-                                come_amount = 0
-                                break
+                        elif 'Мы расторгаем с вами соглашение,' in body:
+                            find_wall = body.rfind('кошелёк')
+                            find_work = body.rfind('работает')
+                            body = body[find_wall:find_work]
+                            body = body.replace('больше не', 'заблокировали! Стоп!📛')
+                            body = body.replace('кошелёк', '⚡️Кошелек')
+                            bot.send_message(message.chat.id, body)
+            except:
+                bot.send_message(message.chat.id, 'Возникла ошибка, проверь данные почты !')
 
-                    elif 'Мы расторгаем с вами соглашение,' in body:
-                        find_wall = body.rfind('кошелёк')
-                        find_work = body.rfind('работает')
-                        body = body[find_wall:find_work]
-                        body = body.replace('больше не', 'заблокировали! Стоп!📛')
-                        body = body.replace('кошелёк', '⚡️Кошелек')
-                        bot.send_message(message.chat.id, body)
         else:
             bot.send_message(message.chat.id, '❌ Доступ запрещен !❌ ')
 
